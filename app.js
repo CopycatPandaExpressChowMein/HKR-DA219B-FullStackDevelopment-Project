81a1d0cae438809a46796a7b29004265b0ab76f1
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2026-05-12 20:40:27 +0200

    Added rudimentary server starting code

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import logger from 'morgan'
+import {ROUTER} from './routes/routes_index.js'
+
+
+const APP = express()
+
+APP.use(logger('dev', {
+    immediate: true,
+    skip: () => process.env.NODE_ENV === 'test'
+}))
+APP.use(express.static('public'))
+APP.use('/', ROUTER)
+
+//TODO: Add eslint 'https://gitlab.com/mikael-roos/node/-/tree/main/src/express5/hello?ref_type=heads#add-a-linter-with-eslint'
+//TODO: Look into and potentially add error handler 
+ 
+const PORT = process.env.PORT || 3001 //Different port from .env 
+APP.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
---
+++ routes/routes_index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+// +----------+ Import Route files here +---------+
+
+// +----------------------------------------------+
+
+const ROUTER = express.Router()
+
+// +----------+ New routes from imports go here +---------+
+
+// +------------------------------------------------------+
+
+
+export {ROUTER}
