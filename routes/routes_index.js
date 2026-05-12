b194e394608525d89dc1de7723ae3a7323207c60
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2026-05-12 20:51:36 +0200

    Added files to handle routes and included them in the route index

--- routes/routes_index.js
+++ routes/routes_index.js
@@ -1,13 +1,17 @@
 import express from 'express'
 
 // +----------+ Import Route files here +---------+
-
+import {ROUTER as WEBPAGEROUTER} from './routes_webpages.js'
+import {ROUTER as LOGINROUTER} from './routes_login.js'
+import {ROUTER as CRUDROUTER} from './routes_CRUD.js'
 // +----------------------------------------------+
 
 const ROUTER = express.Router()
 
 // +----------+ New routes from imports go here +---------+
-
+ROUTER.use('/', WEBPAGEROUTER)
+ROUTER.use('/login', LOGINROUTER)
+ROUTER.use('/CRUD', CRUDROUTER)
 // +------------------------------------------------------+
 
 
