29a8ccac4b36096779202ca428932bcd8362d264
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2026-05-24 01:05:49 +0200

    Resolved further issues from merge and added host requiered by Render deployment

--- routes/routes_index.js
+++ routes/routes_index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 
+// +----------+ New imports go here +---------+
+
 import { ROUTER as LOGINROUTER } from './routes_login.js'
 import { ROUTER as CRUDROUTER } from './routes_CRUD.js'
+import { ROUTER as WEBPAGEROUTER} from './routes_webpages.js'
+
+// +------------------------------------------+
 
 const ROUTER = express.Router()
 
+
+// +----------+ New routes from imports go here +---------+
+
 ROUTER.use('/login', LOGINROUTER)
 ROUTER.use('/CRUD', CRUDROUTER)
+ROUTER.use('/', WEBPAGEROUTER)
 
+// +------------------------------------------------------+
 export { ROUTER }
