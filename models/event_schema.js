dd8a63bcfe5e83b9287cf7147beaacbeaa49cca5
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2026-05-25 15:01:24 +0200

    Revert "Merge pull request #24 from CopycatPandaExpressChowMein:Amjad-Dev-Branch"
    This reverts commit 0400e9455304a27f901b64d011a87ca9d2b29711, reversing
    changes made to 8af837e2130f4c0cf6a16e8d2be414ca7586c22c.

--- models/event_schema.js
+++ models/event_schema.js
@@ -2,29 +2,31 @@ import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 
 const eventSchema = new mongoose.Schema({
-    datetime: Date,
-    eventId: {
-        type: Number,
-        unique: true
-    },
-    type: String,
-    summary: String,
-    location: {
-        name: String,
-        gps: String
-    },
-    url: String,
-    comments: [{
-        author: String,
-        body: String,
-        date: {type: Date, default: Date.now}
-    }],
-    lastUpdate: {type: Date, default: Date.now},
-    archived: {type: Boolean, default: false}
-})
+	datetime: Date,
+	eventId: {
+		type: Number,
+		unique: true,
+	},
+	type: String,
+	summary: String,
+	location: {
+		name: String,
+		gps: String,
+	},
+	url: String,
+	comments: [
+		{
+			author: String,
+			body: String,
+			date: { type: Date, default: Date.now },
+		},
+	],
+	lastUpdate: { type: Date, default: Date.now },
+	archived: { type: Boolean, default: false },
+});
 
-eventSchema.plugin(uniqueValidator)
+eventSchema.plugin(uniqueValidator);
 
-const Event = mongoose.model('event', eventSchema)
+const Event = mongoose.model("event", eventSchema);
 
-export {Event}
+export { Event };
